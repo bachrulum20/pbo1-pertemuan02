ca73183d7bd6be25221077dc98d4f88e8c0b02b6
bachrulum20 <ulum424@gmail.com>
2020-04-26 15:07:02 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ===============");
+  System.out.println("NIDN  : 18630975 ");
+  System.out.println("Nama  : Bahrul Ulum ");
+  System.out.println("=================================");
+  System.out.println();
+ }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,12 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mata Kuliah ===============");
+  System.out.println("NIDN  : TIF3402 ");
+  System.out.println("Nama  : Pemrograman Berbasi Objek 1 ");
+  System.out.println("=================================");
+  System.out.println();
+ }
+}
